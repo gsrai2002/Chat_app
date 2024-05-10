eea80e9b2fc7519546b8c630c5c8c10907668b63
Ghanshyam Rai <gsrai082002@gmail.com>
2024-05-10 10:39:57 +0530

    some more changes

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-i5ss.onrender.com",
     credentials: true,
   },
 });
